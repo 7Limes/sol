83c088f76e2bb3e008cadd71971ac47166109749
7Limes <100952188+7Limes@users.noreply.github.com>
2022-09-09 13:45:44 -0400

    Update and rename calc.sl to calc.sol

--- examples/calc.sol
+++ examples/calc.sol
@@ -5,7 +5,7 @@
 "Enter another number: " i
 "Enter an operation (+, -, *, /): " I_
 
-\ decide which operation to do, jump \
+\ check which operation to do, jump \
 #43=10$?j
 _#45=11$?j
 _#42=12$?j
@@ -29,5 +29,3 @@ _/!0j
 20@
 "Unsupported operation"
 0@
-
-\iiI_#43=1$?j_#45=2$?j_#42=3$?j_#47=4$?j1@_+!0j2@_-!0j3@_*!0j4@_/!0@\
